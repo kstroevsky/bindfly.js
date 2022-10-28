1b36aacf43fc08366c17a253cfa6f12c331d77d1
Ваше Имя <you@example.com>
2022-10-28 13:54:28 +0300

    clear code

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,18 +1,11 @@
-import React, {useState} from "react";
-import {useEffect, useRef} from "react";
+import React from "react";
 import {Particles} from './canvas/animations/Particles';
 import {useAnimation} from "../hooks/useAnimation";
 
 function Animation({properties}) {
-    // const [isLoaded, setIsLoaded] = useState(false);
-    // const canvasRef = useRef(null);
     const {innerWidth, innerHeight} = window;
 
      const canvasRef = useAnimation(Particles, { properties, innerWidth, innerHeight});
-    // console.log(isAnimated)
-    // useEffect(() => {
-    //     setIsLoaded(!!canvasRef.current)
-    // }, [canvasRef.current])
 
       return (
           <>
@@ -25,7 +18,6 @@ function Animation({properties}) {
                 width={innerWidth}
                 height={innerHeight}
             />
-            {/*  <CanvasElement />*/}
           </>
       );
 }
