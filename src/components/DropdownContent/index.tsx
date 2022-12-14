cfe8d639ad28e0729c5883ce7a1c9bde6850fde8
Maksym Yaremenko <maksym.y@develops.today>
2022-12-14 22:34:08 +0200

    feat: optimized list copy and added one line config copy logic

--- src/components/DropdownContent/index.tsx
+++ src/components/DropdownContent/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useCallback, useState } from "react";
-import CheckIcon from "../../assets/Check_icon.svg";
+import CheckIcon from "../../assets/CheckIcon";
+import { COPY_ANIMATION_DURATION } from "../../shared/constants";
 import { IProperty, TPropertiesValues } from "../../shared/types";
 import DropdownItem from "../DropdownItem";
 
@@ -16,30 +17,24 @@ function createEnum<T extends { [P in keyof T]: P }>(o: T) {
 const DropdownContent: FC<IDropdownContent> = ({ propertySet }) => {
   const [isCopied, setIsCopied] = useState(false);
 
-  const onClick = useCallback(() => {
-    navigator.clipboard.writeText(JSON.stringify(propertySet))
+  const onListClick = useCallback(() => {
+    navigator.clipboard.writeText(JSON.stringify(propertySet));
     setIsCopied(true);
 
     setTimeout(() => {
       setIsCopied(false);
-    }, 1000)
+    }, COPY_ANIMATION_DURATION);
   }, [propertySet]);
 
+
   return (
-  <>
-    <ul className="DropdownContent" onClick={onClick}>
+    <ul className="DropdownContent">
+      <CheckIcon width={16} height={16} fill={isCopied ? "#36c373" : "white"} className="Clipboard" onClick={onListClick} />
       {Object.entries<TPropertiesValues>(propertySet).map(
         ([propertyKey, propertyValue]: [string, TPropertiesValues], idx: number) => (
           <DropdownItem key={idx} {...{ propertyKey, propertyValue }} />
         ))}
     </ul>
-    {isCopied && 
-      <div className="Clipboard">
-        <img src={CheckIcon} alt="check icon" />
-        <p>Copied!</p>
-      </div>
-    }
-  </>
 )};
 
 export default DropdownContent;
