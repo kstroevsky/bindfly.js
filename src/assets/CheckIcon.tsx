31b3870e56ef4eb6513b7585cd18c6370c52bcdc
Maksym Yaremenko <maksym.y@develops.today>
2022-12-16 10:58:56 +0200

    fix: remove redudant component

--- src/assets/CheckIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const CheckIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 512 512" {...props}>
-    <path d="M224 0c-35.3 0-64 28.7-64 64v224c0 35.3 28.7 64 64 64h224c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224zM64 160c-35.3 0-64 28.7-64 64v224c0 35.3 28.7 64 64 64h224c35.3 0 64-28.7 64-64v-64h-64v64H64V224h64v-64H64z" />
-  </svg>
-)
-
-export default React.memo(CheckIcon);
